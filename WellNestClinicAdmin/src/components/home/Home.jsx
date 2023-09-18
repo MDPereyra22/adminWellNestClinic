e707307e0ed3c773f2662b0e8d8e690b1c2b4b09
Luciano Fraile <lucianofrailee@gmail.com>
2023-09-17 23:57:47 -0300

    Changes in the Home folder

--- WellNestClinicAdmin/src/components/home/Home.jsx
+++ WellNestClinicAdmin/src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-// Home.js
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import { Link } from 'react-router-dom';
@@ -8,6 +8,7 @@ function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.linkContainer}>
+        <h1>Welcome Admin</h1>
         <Link to="/postUser">Create a new user</Link>
         <Link to="/users">User list</Link>
         <Link to="/postDoctor">Create a new doctor</Link>
