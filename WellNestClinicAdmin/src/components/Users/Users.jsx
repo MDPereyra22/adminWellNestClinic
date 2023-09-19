005f6e09155f54c7d38599974d53025680defd2b
Luciano Fraile <lucianofrailee@gmail.com>
2023-09-19 18:08:01 -0300

    Integration of css to the user list

--- WellNestClinicAdmin/src/components/Users/Users.jsx
+++ WellNestClinicAdmin/src/components/Users/Users.jsx
@@ -1,8 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./Users.module.css";
 import { Link } from "react-router-dom";
-
 
 function Users() {
   const [clients, setClients] = useState([]);
@@ -19,24 +19,39 @@ function Users() {
   }, []);
 
   return (
-    <div className={styles.container}>
-      <Link to="/home" className={styles.link}>Go back </Link>
-      <h2>List of clients</h2>
-      <ul>
-        {clients.map((client, index) => (
-          <li key={index} className={styles.clientItem}>
-            <p className={styles.field}>Name:</p>
-            <p className={styles.value}>{client.name} {client.lastName}</p>
-            <p className={styles.field}>Email address:</p>
-            <p className={styles.value}>{client.email}</p>
-            <p className={styles.field}>DNI:</p>
-            <p className={styles.value}>{client.dni}</p>
-            <p className={styles.field}>Birth date:</p>
-            <p className={styles.value}>{client.birthDate}</p>
-
-          </li>
-        ))}
-      </ul>
+    <div className={styles.componentContainer}>
+      <div className={styles.containerList}>
+        <div className={styles.containerTitle}>
+          <h1>List of clients</h1>
+          <Link to="/home" className={styles.link}>
+            Go back{" "}
+          </Link>
+        </div>
+        <div className={styles.tableContainer}>
+          <table>
+            <thead>
+              <tr>
+                <th>Name:</th>
+                <th>Last Name:</th>
+                <th>Email address:</th>
+                <th>DNI:</th>
+                <th>Birth date:</th>
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map((client, index) => (
+                <tr key={index}>
+                  <td>{client.name}</td>
+                  <td>{client.lastName}</td>
+                  <td>{client.email}</td>
+                  <td>{client.dni}</td>
+                  <td>{client.birthDate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
